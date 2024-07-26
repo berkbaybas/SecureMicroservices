1df0df9473c388cd8c82a8b11e568e856eebd4ac
berk <berk_baybas@hotmail.com>
2024-07-26 15:37:05 +0300

    add IdentityServer quickstart, add ClientCredentials jwt

--- IdentityServer/Program.cs
+++ IdentityServer/Program.cs
@@ -1,16 +1,27 @@
 using IdentityServer;
+using IdentityServerHost.Quickstart.UI;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
 
 builder.Services.AddIdentityServer()
                 .AddInMemoryClients(Config.Clients)
                 .AddInMemoryApiScopes(Config.ApiScopes)
-                .AddInMemoryIdentityResources(Config.IdentityResources)
+                 .AddInMemoryIdentityResources(Config.IdentityResources)
                 .AddTestUsers(Config.TestUsers)
                 .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
+app.UseStaticFiles();
+app.UseRouting();
 app.UseIdentityServer();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapDefaultControllerRoute();
+});
 
 app.Run();
---
+++ Movies.Client/ApiServices/MockMovieApiService.cs
@@ -0,0 +1,45 @@
+﻿using Movies.Client.Models;
+
+namespace Movies.Client.ApiServices
+{
+    public class MockMovieApiService : IMovieApiService
+    {
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            var movieList = new List<Movie>();
+            movieList.Add(
+                new Movie
+                {
+                    Id = 1,
+                    Genre = "Fantastic",
+                    Title = "Harry Potter and Half Blood Prince",
+                    ImageUrl = "images/",
+                    ReleaseDate = DateTime.Now,
+                    Owner = "Berk"
+                });
+
+            return await Task.FromResult(movieList);
+        }
+
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+     
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
